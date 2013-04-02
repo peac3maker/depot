8bb3416142ff7e3fc5d0143fef863c1283af4538
peac3maker <peac3maker@gmx.ch>
2013-04-02 22:58:29 +0200

    added layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with:   %r{\.(gif|jpg|png)$}i,
+    with:   %r{\.(gif|jpg|png|jpeg)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
 end
