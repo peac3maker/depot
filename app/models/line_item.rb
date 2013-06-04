6b453e09fcb9a4ffd374104abdb70cae5dbb4f76
peac3maker <peac3maker@gmx.ch>
2013-06-04 11:32:32 +0200

    ajax

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ActiveRecord::Base
 	belongs_to :product
 	belongs_to :cart
-	attr_accessible :product, :cart,:product_id
+	attr_accessible :product, :cart,:product_id,:quantity
 
 	def total_price
 		product.price * quantity
